540e694f1e9ccac6d6e1494686b9b403d7d3cbec
tiwka19 <61656257+tiwka19@users.noreply.github.com>
2021-02-24 07:34:36 +0200

    --

--- js/main.js
+++ js/main.js
@@ -3,6 +3,52 @@ $(function(){
         arrows : false,
         dots : true, 
         autoplay : false,
-        fade : true
+        fade : true,
+        responsive: [
+            {
+                breakpoint : 1200, 
+                settings : {
+                    dots : false
+                }
+            },
+        ]
     });
+
+    $('.reviews__slider').slick({
+        arrows : false,
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      $('.menu__btn').on('click', function(){
+          $('.menu__list').toggleClass('menu__list--active')
+      });
 });
